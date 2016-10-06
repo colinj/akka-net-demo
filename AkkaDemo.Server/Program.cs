74b9d58338e01f6cfbfbb8fd6ba3a22b789d3516
Colin Johnsun <colin.johnsun@boardroomlimited.com.au>
2016-10-07 10:34:35 +1100

    Reorganise the projects.

--- AkkaDemo.Server/Program.cs
+++ AkkaDemo.Server/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Akka.Actor;
 using AkkaDemo.Common;
 using AkkaDemo.Common.Actors;
 using AkkaDemo.Common.Messages;
 
-namespace AkkaDemo
+namespace AkkaDemo.Server
 {
     internal class Program
     {
