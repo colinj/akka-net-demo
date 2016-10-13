84a6ea9b457fd59ec7ea377b07c76b6fa836473e
Colin Johnsun <colin.johnsun@gmail.com>
2016-10-13 13:46:10 +1100

    Add life cycle hooks to actors

--- AkkaDemo.Common/Actors/LogCoordinatorActor.cs
+++ AkkaDemo.Common/Actors/LogCoordinatorActor.cs
@@ -8,13 +8,13 @@ namespace AkkaDemo.Common.Actors
 {
     public class LogCoordinatorActor : ReceiveActor
     {
+        private const string ActorTypeDesc = "LogCoordinatorActor";
         private const string LogPath = @"C:\AkkaLogDemo\";
         private readonly Dictionary<string, IActorRef> _loggers;
 
         public LogCoordinatorActor()
         {
             Directory.CreateDirectory(LogPath);
-
             _loggers = new Dictionary<string, IActorRef>();
 
             Receive<LogEntryMessage>(msg => HandleLogEntryMessage(msg));
@@ -31,7 +31,7 @@ namespace AkkaDemo.Common.Actors
             if (!_loggers.ContainsKey(appId))
             {
                 var fileName = LogPath + appId + ".txt";
-                var newLogEntryActor = Context.ActorOf(Props.Create(() => new LoggerActor(fileName)), "Logger_" + appId);
+                var newLogEntryActor = Context.ActorOf(Props.Create(() => new LoggerActor(appId, fileName)), "Logger_" + appId);
 
                 _loggers.Add(appId, newLogEntryActor);
             }
@@ -39,9 +39,28 @@ namespace AkkaDemo.Common.Actors
             return _loggers[appId];
         }
 
+        #region Lifecycle hooks
         protected override void PreStart()
         {
-            ColorConsole.WriteLineGreen("LogCoordinator PreStart");
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } PreStart");
         }
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } PostStop");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } PreRestart because: { reason }");
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } PostRestart because: { reason }");
+            base.PostRestart(reason);
+        }
+        #endregion
     }
 }
--- AkkaDemo.Common/Actors/LoggerActor.cs
+++ AkkaDemo.Common/Actors/LoggerActor.cs
@@ -8,10 +8,13 @@ namespace AkkaDemo.Common.Actors
 {
     public class LoggerActor : ReceiveActor
     {
+        private const string ActorTypeDesc = "LoggerActor";
+        private readonly string _appId;
         private readonly string _logFileName;
 
-        public LoggerActor(string logFileName)
+        public LoggerActor(string appId, string logFileName)
         {
+            _appId = appId;
             _logFileName = logFileName;
             Receive<LogEntryMessage>(msg => HandleLogEntryMessage(msg));
         }
@@ -22,9 +25,28 @@ namespace AkkaDemo.Common.Actors
             ColorConsole.WriteLineYellow($"Message logged for LoggerActor '{ msg.AppId }'.");
         }
 
+        #region Lifecycle hooks
         protected override void PreStart()
         {
-            ColorConsole.WriteLineGreen($"LoggerActor: Log file = { _logFileName }.");
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc }: Log file = { _logFileName }.");
         }
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } { _appId } PostStop");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } { _appId } PreRestart because: { reason }");
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineYellow($"{ ActorTypeDesc } { _appId } PostRestart because: { reason }");
+            base.PostRestart(reason);
+        }
+        #endregion
     }
 }
